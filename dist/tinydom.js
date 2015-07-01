b6cf156ac0a433429ccff1297f89da9f410ccfdb
Commander-lol <ljcapitanio@gmail.com>
2015-07-01 01:52:16 +0100

    Updated json.merge to better handle undefined input

--- dist/tinydom.js
+++ dist/tinydom.js
@@ -217,6 +217,13 @@ tinyDOM.triggerOn = function (target, eventName, data, bubbles, cancelable) {
 };
 
 tinyDOM.json = {
+    /**
+     * Gets the keyset of an object
+     * @param   {Object} json The object whose keys are to be
+     *                      retrieved
+     * @returns {Array}  An array of Strings corresponding to
+     *                   the properties of an object
+     */
     keys: function(json) {
         var kys = [],
             indx;
@@ -227,6 +234,13 @@ tinyDOM.json = {
         }
         return kys;
     },
+    /**
+     * Determines if the parameter represents a valid json object
+     * @param   {String}  obj The string to be checked regarding its
+     *                      validity as a JSON object
+     * @returns {Boolean} Whether or not obj represents a valid json
+     *                    object
+     */
     is: function (obj) {
         try {
             JSON.parse(obj);
@@ -235,9 +249,22 @@ tinyDOM.json = {
             return false;
         }
     },
+    /**
+     * Takes two objects and adds the properties of the second to the
+     * first, with those of the second taking presedence in cases where
+     * both objects intersect
+     * @param   {Object} json1 The first object to combine
+     * @param   {Object} json2 The second object to combine
+     * @returns {Object} If json1 is undefined, returns json2. Otherwise returns
+     *                   json1. In cases where json2 is undefined, json1 will be
+     *                   unchanged, otherwise json1 will contain all the properties
+     *                   of json2.
+     */
     merge: function (json1, json2) {
-		if (!this.exists(json1) || !this.exists(json2)) {
-			return null;
+		if (!this.exists(json1)) {
+            return json2;
+        } else if (!this.exists(json2)) {
+			return json1;
 		} else {
             var prop;
 			for (prop in json2) {
